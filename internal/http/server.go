ee1fb9e72b5b1ccd94bac28c59d39a32c900e215
nnaakkaaii <ultraganbayu10@gmail.com>
2022-10-20 00:05:33 +0900

    implement server.Server

---
+++ internal/http/server.go
@@ -0,0 +1,36 @@
+package http
+
+import (
+	"context"
+	"fmt"
+	"github.com/nnaakkaaii/tododemo/internal/db"
+	"net/http"
+)
+
+type Server struct {
+	server *http.Server
+}
+
+func NewServer(port int, d db.DB) *Server {
+	mux := http.NewServeMux()
+	return &Server{
+		server: &http.Server{
+			Addr:    fmt.Sprintf(":%d", port),
+			Handler: mux,
+		},
+	}
+}
+
+func (s *Server) Start() error {
+	if err := s.server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		return fmt.Errorf("failed to server: %w", err)
+	}
+	return nil
+}
+
+func (s *Server) Stop(ctx context.Context) error {
+	if err := s.server.Shutdown(ctx); err != nil {
+		return fmt.Errorf("failed to shutdown: %w", err)
+	}
+	return nil
+}
